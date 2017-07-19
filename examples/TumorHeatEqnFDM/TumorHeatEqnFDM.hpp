97e35361f0a7b7018710a3f977596d8b0837b5dc
Martin Stoeckl <martin.stoeckl@mailbox.tu-dresden.de>
2017-07-19 16:12:16 +0200

    Deleted doxygen comments in TumorHeatEqnFDM example
    Deleted doxygen comments in TumorHeatEqnFDM.cpp since there are for the HeatEqnFDM.

--- examples/TumorHeatEqnFDM/TumorHeatEqnFDM.hpp
+++ examples/TumorHeatEqnFDM/TumorHeatEqnFDM.hpp
@@ -7,7 +7,7 @@
 /**
  *  @file TumorHeatEqnFDM.hpp
  *
- *  @brief Implementation of n-dimensional heat equation problem on unit plane.
+ *  @brief Implementation of n-dimensional Pennes bio heat equation problem on unit cube.
  */
 
 #include <iostream>
@@ -19,77 +19,13 @@
  * \class TumorHeatEqnFDM
  *  @brief Class for discretized heat equation problem.
  *
- * \section heatEqnFDM 2D Heat Equation Problem on unit plane
+ * \section tumorHeatEqnFDM 3D Pennes Bioheat Equation Problem on unit cube
  *
  *
  * \subsection mathdescr Mathematical Description
- * Given:
- * <ul>
- * <li> Time interval \f[[t_S; t_E] \mbox{ with } 0 \le t_S < t_E,\f] </li>
- * <li> domain \f[\Omega := (0,1)^3,\f] </li>
- * <li> source \f[f: \bar{\Omega} \times (t_S;t_E] \to R, \quad
-        f(x,t) := 0,\f] </li>
- * <li> boundary condition \f[g: \partial\Omega \times (t_S;t_E] \to R, \quad
-         g(x,t) := 0,\f] </li>
- * <li> initial condition \f[\widetilde{y}: \bar{\Omega} \to R, \quad
-        \widetilde{y}(x) :=  \prod_{i=0}^{d-1} x_i \cdot
-                    (x_i - 1/2)^2 \cdot (1 - x_i),\f] </li>
- * </ul>
- * Find \f[y: \bar{\Omega} \times [t_S; t_E] \to {R}\f] such that
- * \f{eqnarray*}{
- * \partial_t y - \Delta y & =&  f  \quad \mbox{in }
- *          \Omega \times (t_S;t_E], \\
- *                      y & =&  g \quad \mbox{on }
- *          \partial\Omega \times (t_S;t_E], \\
- *             y(\cdot,t_S) & =& \widetilde{y}  \quad \mbox{in } \bar{\Omega}.
- * \f}
- *
  * \subsection mathdiscretization Discretization of the problem
  * \subsubsection discrTD Discretization of the time interval and the domain
- * Let the time interval [t_S;t_E] be uniformly discretised with
- * \f[t_l := \{ t_S + l \cdot \tau\}_l \f]
- * with time step size tau > 0.
- *
- * Let the domain Omega be uniformly discretised with
- * \f[ x_{(i,j,k)} :=  \{ (i \cdot h_0, j \cdot h_1, k \cdot h_2) \}_{(i,j,k)}
- * \f]
- * with grid sizes h_p>0 for all p.
- * <ul>
- * <li>G_I: Set of all interior grid nodes, </li>
- * <li>G_B: Set of all boundary grid nodes,</li>
- * <li>G := G_I with G_B: Set of all grid nodes</li>
- * </ul>
- *
- * \subsubsection discrHeatEqn Discretization of the heat equation
- * Define the following vectors:
- * \f{eqnarray*}{
- * Y^{(l)}_{(i,j,k)} &:=& y(x_{(i,j,k)},t_l), \\
- * F^{(l)}_{(i,j,k)} &:=& f(x_{(i,j,k)},t_l), \\
- * G^{(l)}_{(i,j,k)} &:=& g(x_{(i,j,k)},t_l), \\
- * \widetilde{Y}_{(i,j,k)} &:=& \widetilde{y}(x_{(i,j,k)}) \\
- * && \quad \mbox{for all } t_l \in \tau_h, x_{(i,j,k)} \in \Omega_h.
- * \f}
- * Discretise derivatives in space using the symmetric difference quotients
- * (Finite Difference Method with 7-point stencil) and in time using the
- * forward difference quotient (explicit Euler scheme):
- * Discretise in space (Finite Difference Method with 7-point stencil)
- * and in time (explicit Euler scheme):
- * \f{eqnarray*}{
- * Y^{(l+1)}_{(i,j,k)} &=&  Y^{(l)}_{(i,j,k)}
- *                   + \big( Y^{(l)}_{(i-1,j,k)} - 2 \cdot Y^{(l)}_{(i,j,k)}
- *               + Y^{(l)}_{(i+1,j,k)} \big) \cdot \tau / h_0^2   \\
- * &&
- *               \quad  \quad \quad  \quad  + \big(  Y^{(l)}_{(i,j-1,k)} - 2 \cdot Y^{(l)}_{(i,j,k)}
- *               + Y^{(l)}_{(i,j+1,k)} \big) \cdot \tau / h_1^2   \\
- * &&            \quad \quad  \quad \quad  + \big( Y^{(l)}_{(i,j,k-1)}  - 2 \cdot Y^{(l)}_{(i,j,k)}
- *               + Y^{(l)}_{(i,j,k+1)} \big) \cdot \tau / h_2^2  \\
- * &&  \quad \quad \quad\quad + \tau \cdot F^{(l)}_{(i,j,k)}  \quad\quad
- * \forall l, \, \forall {(i,j,k)} \in \cal{G}_I, \\
- * Y^{(l+1)}_{(i,j,k)} &=&  G^{(l+1)}_{(i,j,k)}
- *               \quad   \quad \forall l, \, \forall {(i,j,k)} \in \cal{G}_B, \\
- *            Y^{(0)}_{(i,j,k)} & =&  \widetilde{Y}_{(i,j,k)}\quad \quad
- * \forall {(i,j,k)} \in \cal{G}.
- * \f}
+ * \subsubsection discrHeatEqn Discretization of Pennes bioheat equation
 */
 template<typename CT, std::size_t DIM>
 class TumorHeatEqnFDM : public ScaFES::Problem<TumorHeatEqnFDM<CT,DIM>, CT, DIM> {
