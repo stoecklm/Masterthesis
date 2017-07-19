42dab1e141e7f6904a71ad52e142ba33d766845c
Martin Stoeckl <martin.stoeckl@mailbox.tu-dresden.de>
2017-07-19 16:16:12 +0200

    Changed dim to 3 in TumorHeatEqnFDM example
    Changed DIM back from 2 to 3 in TumorHeatEqnFDM example.
    Modified variables in main.cpp and RUN.sh.in.

--- examples/TumorHeatEqnFDM/main.cpp
+++ examples/TumorHeatEqnFDM/main.cpp
@@ -8,7 +8,7 @@
 #include "TumorHeatEqnFDM.hpp"
 
 /** Space dimension of problem. */
-const int DIM = 2;
+const int DIM = 3;
 
 /** Main program for TumorHeatEqnFDM. */
 int main(int argc, char *argv[]) {
@@ -48,4 +48,3 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
-
