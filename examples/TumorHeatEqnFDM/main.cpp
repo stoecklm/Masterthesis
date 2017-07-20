deabd8751d7c9d1e3804817c33bf555828958787
Martin Stoeckl <martin.stoeckl@mailbox.tu-dresden.de>
2017-07-20 11:15:55 +0200

    Added new vectors in TumorHeatEqnFDM example
    Modified main.cpp file.
    Deleted old vectors and added new vectors for variables needed in Pennes bioheat equation.

--- examples/TumorHeatEqnFDM/main.cpp
+++ examples/TumorHeatEqnFDM/main.cpp
@@ -14,31 +14,49 @@ const int DIM = 3;
 int main(int argc, char *argv[]) {
     ScaFES::Parameters paramsCl(argc, argv);
     ScaFES::GridGlobal<DIM> gg(paramsCl);
-    std::vector<std::string> nameDatafield(3);
-    nameDatafield[0] = "F";
-    nameDatafield[1] = "G";
-    nameDatafield[2] = "U";
-    std::vector<int> stencilWidth(3);
-    stencilWidth[0] = 0;
+    std::vector<std::string> nameDatafield(6);
+    nameDatafield[0] = "T"; /* Temperature vector. */
+    nameDatafield[1] = "rho"; /* rho (density) vector. */
+    nameDatafield[2] = "c"; /* c (specific heat capacity) vector. */
+    nameDatafield[3] = "lambda"; /* lambda (thermal conductivity) vector. */
+    nameDatafield[4] = "w"; /* w (perfusion) vector. */
+    nameDatafield[5] = "Q_m"; /* Q_m (metabolism) vector. */
+    std::vector<int> stencilWidth(6);
+    stencilWidth[0] = 1;
     stencilWidth[1] = 0;
-    stencilWidth[2] = 1;
-    std::vector<bool> isKnownDf(3);
-    isKnownDf[0] = true;
-    isKnownDf[1] = true;
+    stencilWidth[2] = 0;
+    stencilWidth[3] = 0;
+    stencilWidth[4] = 0;
+    stencilWidth[5] = 0;
+    std::vector<bool> isKnownDf(6);
+    isKnownDf[0] = false;
+    isKnownDf[1] = false;
     isKnownDf[2] = false;
-    std::vector<int> nLayers(3);
+    isKnownDf[3] = false;
+    isKnownDf[4] = false;
+    isKnownDf[5] = false;
+    std::vector<int> nLayers(6);
     nLayers[0] = 0;
     nLayers[1] = 0;
     nLayers[2] = 0;
-    std::vector<double> defaultValue(3);
+    nLayers[3] = 0;
+    nLayers[4] = 0;
+    nLayers[5] = 0;
+    std::vector<double> defaultValue(6);
     defaultValue[0] = 0.0;
     defaultValue[1] = 0.0;
     defaultValue[2] = 0.0;
-    std::vector<ScaFES::WriteHowOften> writeToFile(3, ScaFES::WriteHowOften::LIKE_GIVEN_AT_CL);
-    std::vector<bool> computeError(3);
+    defaultValue[3] = 0.0;
+    defaultValue[4] = 0.0;
+    defaultValue[5] = 0.0;
+    std::vector<ScaFES::WriteHowOften> writeToFile(6, ScaFES::WriteHowOften::LIKE_GIVEN_AT_CL);
+    std::vector<bool> computeError(6);
     computeError[0] = false;
     computeError[1] = false;
     computeError[2] = false;
+    computeError[3] = false;
+    computeError[4] = false;
+    computeError[5] = false;
     std::vector<double> geomparamsInit;
 
     TumorHeatEqnFDM<double, DIM> ppp(paramsCl, gg, false, nameDatafield, stencilWidth,
