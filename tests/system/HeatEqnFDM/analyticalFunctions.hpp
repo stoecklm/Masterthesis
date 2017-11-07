e24c274146b968554ff1d326be60158693595d50
Martin Stoeckl <martin.stoeckl@mailbox.tu-dresden.de>
2017-11-07 12:07:58 +0100

    Added timeLinSpaceConst to analytical functions

--- tests/system/HeatEqnFDM/analyticalFunctions.hpp
+++ tests/system/HeatEqnFDM/analyticalFunctions.hpp
@@ -20,6 +20,14 @@ TT timeLin(TT const& t) {
     return (1.0 + t);
 }
 
+/** Calculates value of a constant space function for given grid point.
+ *  @param x Coordinates of given grid point.
+ */
+template<typename TT, std::size_t DIM>
+TT spaceConst(ScaFES::Ntuple<TT,DIM> const& /*x*/) {
+    return 1.0;
+}
+
 /** Calculates value of a linear space function for given grid point.
  *  @param x Coordinates of given grid point.
  */
@@ -33,6 +41,15 @@ TT spaceLin(ScaFES::Ntuple<TT,DIM> const& x) {
 }
 
 /** Calculates value of time derivative of a linear time and
+ *  a constant space function for given grid point.
+ *  @param x Coordinates of given grid point.
+ */
+template<typename TT, std::size_t DIM>
+TT timeLinSpaceConstdTime(ScaFES::Ntuple<TT,DIM> const& x) {
+    return spaceConst<TT,DIM>(x);
+}
+
+/** Calculates value of time derivative of a linear time and
  *  a linear space function for given grid point.
  *  @param x Coordinates of given grid point.
  */
@@ -42,7 +59,19 @@ TT timeLinSpaceLindTime(ScaFES::Ntuple<TT,DIM> const& x) {
 }
 
 /** Calculates value of first order space derivative of a linear time
- *  and a linear pace function for given grid point, time and orientation.
+ *  and a constant space function for given grid point, time and orientation.
+ *  @param x Coordinates of given grid point.
+ *  @param t Value of absolute time.
+ *  @param currDIM Current dimension (orientation of points).
+ */
+template<typename TT, std::size_t DIM>
+TT timeLinSpaceConstdSpace1stOrder(ScaFES::Ntuple<TT,DIM> const& /*x*/,
+                                   TT const& /*t*/, std::size_t /*currDIM*/) {
+    return 0.0;
+}
+
+/** Calculates value of first order space derivative of a linear time
+ *  and a linear space function for given grid point, time and orientation.
  *  @param x Coordinates of given grid point.
  *  @param t Value of absolute time.
  *  @param currDIM Current dimension (orientation of points).
@@ -51,6 +80,17 @@ template<typename TT, std::size_t DIM>
 TT timeLinSpaceLindSpace1stOrder(ScaFES::Ntuple<TT,DIM> const& /*x*/,
                                  TT const& t, std::size_t /*currDIM*/) {
     return timeLin<TT>(t);
+}
+
+/** Calculates sum of second order space derivatives for all dimensions of
+ *  a linear time and a constant space function for given grid point and time.
+ *  @param x Coordinates of given grid point.
+ *  @param t Value of absolute time.
+ */
+template<typename TT, std::size_t DIM>
+TT timeLinSpaceConstSumOfdSpace2ndOrder(ScaFES::Ntuple<TT,DIM> const& /*x*/,
+                                        TT const& /*t*/) {
+    return 0.0;
 }
 
 /** Calculates sum of second order space derivatives for all dimensions
@@ -62,6 +102,16 @@ template<typename TT, std::size_t DIM>
 TT timeLinSpaceLinSumOfdSpace2ndOrder(ScaFES::Ntuple<TT,DIM> const& /*x*/,
                                       TT const& /*t*/) {
     return 0.0;
+}
+
+/** Calculates value of a linear time and a constant space function
+ *  for given grid point and time.
+ *  @param x Coordinates of given grid point.
+ *  @param t Value of absolute time.
+ */
+template<typename TT, std::size_t DIM>
+TT timeLinSpaceConstFunc(ScaFES::Ntuple<TT,DIM> const& x, TT const& t) {
+    return (timeLin<TT>(t) * spaceConst<TT,DIM>(x));
 }
 
 /** Calculates value of a linear time and a linear space function
