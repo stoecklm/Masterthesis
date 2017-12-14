25bad955a9629e0b6b963b38c5d759ae83b4b34a
Martin Stoeckl <martin.stoeckl@mailbox.tu-dresden.de>
2017-12-14 15:13:40 +0100

    Started changing Tumor heat example
    Deleted old functions since these are before from Pennes Bioheat Equation was tested in the system test.
    Changed and renamed parameters (member variables) to the values and names from Bousselham (2017) paper.
    Changed dimensions.
    Started working to create if/else condition for distinguish between tumor and healty tissue assuming tumor is a sphere.

--- examples/TumorHeatEqnFDM/TumorHeatEqnFDM.hpp
+++ examples/TumorHeatEqnFDM/TumorHeatEqnFDM.hpp
@@ -1,5 +1,5 @@
 /* ScaFES
- * Copyright (c) 2011-2015, ZIH, TU Dresden, Federal Republic of Germany.
+ * Copyright (c) 2017, ZIH, TU Dresden, Federal Republic of Germany.
  * For details, see the files COPYING and LICENSE in the base directory
  * of the package.
  */
@@ -7,7 +7,7 @@
 /**
  *  @file TumorHeatEqnFDM.hpp
  *
- *  @brief Implementation of n-dimensional Pennes bio heat equation problem on unit cube.
+ *  @brief Implementation of a 3-dimensional Pennes bioheat equation problem.
  */
 
 #include <iostream>
@@ -17,74 +17,79 @@
  ******************************************************************************/
 /**
  * \class TumorHeatEqnFDM
- *  @brief Class for discretized heat equation problem.
+ *  @brief Class for discretized Pennes bioheat equation problem.
  *
- * \section tumorHeatEqnFDM 3D Pennes Bioheat Equation Problem on unit cube
- *
- *
- * \subsection mathdescr Mathematical Description
- * \subsection mathdiscretization Discretization of the problem
- * \subsubsection discrTD Discretization of the time interval and the domain
- * \subsubsection discrHeatEqn Discretization of Pennes bioheat equation
 */
 template<typename CT, std::size_t DIM>
 class TumorHeatEqnFDM : public ScaFES::Problem<TumorHeatEqnFDM<CT,DIM>, CT, DIM> {
   public:
 
-    /** constant rho_brain. Material parameter (density) for the brain. */
-    const double RHO_BRAIN = 1040.0; /* kg/m^3 */
+    /* Values from Bousselham et al. (2017). */
 
-    /** constant c_brain. Material parameter (specific heat capacity)
-     * for the brain. */
-    const double C_BRAIN = 3650.0; /* J/(kg K) */
+    /************************************************************************/
+    /* Values from Table 1: Thermophysical properties. */
+    /** constant rho. Density. */
+    const double RHO = 1040.0; /* kg/m^3 */
 
-    /** constant lambda_brain. Material parameter (thermal conductivity)
-     * for the brain. */
-    const double LAMBDA_BRAIN = 0.6; /* W/(m K) */
+    /** constant C. Specific heat capacity. */
+    const double C = 3650.0; /* J/(kg K) */
 
-    /** constant rho_blood. Material parameter (density) for blood. */
-    const double RHO_BLOOD = 1047.0; /* kg/m^3 */
+    /** constant K. Thermal conductivity). */
+    const double K = 0.6; /* W/(m K) */
 
-    /** constant c_blood. Material parameter (specific heat capacity)
-     * for blood. */
-    const double C_BLOOD = 3600.0; /* J/(kg K) */
+    /** constant Q. Metabolism heat generation. */
+    const double Q = 25000.0; /* W/(m^3) */
 
-    /** constant w_brain. Material parameter (perfusion) for the brain. */
-    const double W_BRAIN = 0.0399; /* 1/s */
+    /** constant rho_b. Density of the blood. */
+    const double RHO_B = 1052.0; /* kg/m^3 */
 
-    /** constant T_brain_init. Parameter (temperature) for
-     * initalization of brain temperature. */
-    const double T_BRAIN_INIT = 306.25; /* K */
+    /** constant C_Pb. Specific heat of the blood. */
+    const double C_PB = 3800.0; /* J/(kg K) */
 
-    /** constant T_tumor_init. Parameter (temperature) for
-     * initalization of tumor temperature.
-     * Tumor = Anaplastic strocytoma. */
-    const double T_TUMOR_INIT = 312.35; /* K */
+    /** constant omega_b. Blood perfusion rate (normal brain tissue). */
+    const double OMEGA_B_BRAIN = 0.004; /* 1/s */
 
-    /** constant T_blood. Parameter (temperature) for blood. */
-    const double T_BLOOD = 310.15; /* K */
+    /** constant omega_b. Blood perfusion rate (Astrocytoma brain tumor).
+      * Table 1 presents an interval. Exact value is given in text.
+      */
+    const double OMEGA_B_TUMOR = 0.0007; /* 1/s */
 
-    /** constant T_amb. Parameter (temperature) for ambient. */
-    const double T_AMB = 291.15; /* K */
+    /************************************************************************/
+    /* Values given in text. */
+    /** constant T_i. Initial condition for T. */
+    const double T_I = 37.0; /* K */
 
-    /** constant alpha_amb. Parameter (heat transfer coefficent)
-     for ambient. */
-    const double ALPHA_AMB = 10.0; /* W/(m^2 K) */
+    /** constant h. Ambient convetion. */
+    const double H = 10.0; /* W/(m^2 K) */
 
-    /** constant q_M_brain. Material parameter (metabolism) for the brain. */
-    const double Q_DOT_M_BRAIN = 49937.0; /* W/(m^3) */
+    /** constant T_inf. Air temperature. */
+    const double T_inf = 22.4; /* K */
 
-    /** constant sphereRadius. Geometry parameter (radius) for tumor. */
-    const double sphereRadius = 0.025;
+    /************************************************************************/
+    /* Values varied in text. */
+    /** constant diameter. diameter of the tumor. */
+    const double DIAMETER = 0.02; /* m */
 
-    /** constant x0. Geometry parameter (x coordiante) for tumor. */
-    const double x0 = 0.05;
+    /** constant depth. depth of the tumor. */
+    const double DEPTH = 0.008; /* m */
 
-    /** constant y0. Geometry parameter (y coordiante) for tumor. */
-    const double y0 = 0.05;
+    /************************************************************************/
+    /* Values missing in text. */
+    /* constant T_a. Temperature of artery.
+     * Value from Das et al.: Numerical estimation... (2013). */
+    const double T_a = 37.0; /* K */
 
-    /** constant z0. Geometry parameter (z coordiante) for tumor. */
-    const double z0 = 0.05;
+    /************************************************************************/
+    /* Auxiliary values. */
+    /* Center of tumor = (x0, y0, z0). */
+    const double x0 = this->params().coordNodeLast()[0]/2.0;
+    const double y0 = this->params().coordNodeLast()[1]/2.0;
+    const double z0 = this->params().coordNodeLast()[2]-DEPTH;
+
+    /* Radius of tumor. */
+    const double RADIUS = DIAMETER/2.0;
+
+    /************************************************************************/
 
     /** All fields which are related to the underlying problem
      * are added in terms of an entry of the parameters of
@@ -103,22 +108,22 @@ class TumorHeatEqnFDM : public ScaFES::Problem<TumorHeatEqnFDM<CT,DIM>, CT, DIM>
      * @param geomparamsInit Initial guess of geometrical parameters.
      */
     TumorHeatEqnFDM(ScaFES::Parameters const& params,
-               ScaFES::GridGlobal<DIM> const& gg,
-               bool useLeapfrog,
-               std::vector<std::string> const& nameDatafield,
-               std::vector<int> const& stencilWidth,
-               std::vector<bool> const& isKnownDf,
-               std::vector<int> const& nLayers = std::vector<int>(),
-               std::vector<CT> const& defaultValue = std::vector<CT>(),
-               std::vector<ScaFES::WriteHowOften> const& writeToFile
-                 = std::vector<ScaFES::WriteHowOften>(),
-               std::vector<bool> const& computeError = std::vector<bool>(),
-               std::vector<CT> const& geomparamsInit = std::vector<CT>() )
+                    ScaFES::GridGlobal<DIM> const& gg,
+                    bool useLeapfrog,
+                    std::vector<std::string> const& nameDatafield,
+                    std::vector<int> const& stencilWidth,
+                    std::vector<bool> const& isKnownDf,
+                    std::vector<int> const& nLayers = std::vector<int>(),
+                    std::vector<CT> const& defaultValue = std::vector<CT>(),
+                    std::vector<ScaFES::WriteHowOften> const& writeToFile
+                      = std::vector<ScaFES::WriteHowOften>(),
+                    std::vector<bool> const& computeError = std::vector<bool>(),
+                    std::vector<CT> const& geomparamsInit = std::vector<CT>() )
         : ScaFES::Problem<TumorHeatEqnFDM<CT, DIM>, CT, DIM>(params, gg, useLeapfrog,
-                                                        nameDatafield, stencilWidth,
-                                                        isKnownDf, nLayers,
-                                                        defaultValue, writeToFile,
-                                                        computeError, geomparamsInit)
+                                                             nameDatafield, stencilWidth,
+                                                             isKnownDf, nLayers,
+                                                             defaultValue, writeToFile,
+                                                             computeError, geomparamsInit)
         { }
 
     /** Evaluates all fields at one given global inner grid node.
@@ -128,12 +133,22 @@ class TumorHeatEqnFDM : public ScaFES::Problem<TumorHeatEqnFDM<CT,DIM>, CT, DIM>
     void evalInner(std::vector< ScaFES::DataField<CT, DIM> >& vNew,
                    ScaFES::Ntuple<int,DIM> const& idxNode,
                    int const& /*timestep*/) {
-        vNew[0](idxNode) = RHO_BRAIN;     /* knownDf(0, idxNode). rho.     */
-        vNew[1](idxNode) = C_BRAIN;       /* knownDf(1, idxNode). c.       */
-        vNew[2](idxNode) = LAMBDA_BRAIN;  /* knownDf(2, idxNode). lambda.  */
-        vNew[3](idxNode) = W_BRAIN;       /* knownDf(3, idxNode). w.       */
-        vNew[4](idxNode) = Q_DOT_M_BRAIN; /* knownDf(4, idxNode). q_dot_m. */
-        /* vNew[5](idxNode) = 0.0;           knownDf(5, idxNode). T.       */
+        /* Get coordinates of current node. */
+        ScaFES::Ntuple<double,DIM> x = this->coordinates(idxNode);
+
+        /* Calculate distance to tumor center. */
+        double distance = ((x[0] - this->x0) * (x[0] - this->x0))
+                          + ((x[1] - this->y0) * (x[1] - this->y0))
+                          + ((x[2] - this->z0) * (x[2] - this->z0));
+
+        /* Check if current point is inside tumor. */
+        if (distance <= (this->RADIUS*this->RADIUS)) {
+            /* Inside tumor. */
+            vNew[0](idxNode) = OMEGA_B_TUMOR;
+        } else {
+            /* Outside tumor, i.e. normal healthy brain tissue. */
+            vNew[0](idxNode) = OMEGA_B_BRAIN;
+        }
     }
 
     /** Evaluates all fields at one given global border grid node.
@@ -144,12 +159,8 @@ class TumorHeatEqnFDM : public ScaFES::Problem<TumorHeatEqnFDM<CT,DIM>, CT, DIM>
     void evalBorder(std::vector< ScaFES::DataField<CT, DIM> >& vNew,
                     ScaFES::Ntuple<int,DIM> const& idxNode,
                     int const& timestep) {
-        vNew[0](idxNode) = RHO_BRAIN;     /* knownDf(0, idxNode). rho.     */
-        vNew[1](idxNode) = C_BRAIN;       /* knownDf(1, idxNode). c.       */
-        vNew[2](idxNode) = LAMBDA_BRAIN;  /* knownDf(2, idxNode). lambda.  */
-        vNew[3](idxNode) = W_BRAIN;       /* knownDf(3, idxNode). w.       */
-        vNew[4](idxNode) = Q_DOT_M_BRAIN; /* knownDf(4, idxNode). q_dot_m. */
-        /* vNew[5](idxNode) = 0.0;           knownDf(5, idxNode). T.       */
+        /* Assuming border is always healthy brain tissue. */
+        vNew[0](idxNode) = OMEGA_B_BRAIN;
     }
 
     /** Initializes all unknown fields at one given global inner grid node.
@@ -161,32 +172,17 @@ class TumorHeatEqnFDM : public ScaFES::Problem<TumorHeatEqnFDM<CT,DIM>, CT, DIM>
                    std::vector<TT> const& /*vOld*/,
                    ScaFES::Ntuple<int,DIM> const& idxNode,
                    int const& /*timestep*/) {
-        double sphereCo =
-            (this->coordinates(idxNode).elem(0) - x0)
-            * (this->coordinates(idxNode).elem(0) - x0)
-            + (this->coordinates(idxNode).elem(1) - y0)
-            * (this->coordinates(idxNode).elem(1) - y0)
-            + (this->coordinates(idxNode).elem(2) - z0)
-            * (this->coordinates(idxNode).elem(2) - z0);
-        if (sphereCo <= (sphereRadius*sphereRadius)) {
-            vNew[0](idxNode) = T_TUMOR_INIT; /* knownDf(0, idxNode). T. */
-        } else {
-            vNew[0](idxNode) = T_BRAIN_INIT; /* knownDf(0, idxNode). T. */
-        }
     }
 
     /** Initializes all unknown fields at one given global border grid node.
      *  @param vNew Set of all unknown fields (return value).
-     *  @param vOld Set of all given fields.
      *  @param idxNode Index of given grid node.
-     *  @param timestep Given time step.
      */
     template<typename TT>
     void initBorder(std::vector< ScaFES::DataField<TT, DIM> >& vNew,
                    std::vector<TT> const& /*vOld*/,
                    ScaFES::Ntuple<int,DIM> const& idxNode,
                    int const& /*timestep*/) {
-        vNew[0](idxNode) = T_BRAIN_INIT; /* knownDf(0, idxNode). T. */
     }
 
     /** Updates all unknown fields at one given global inner grid node.
@@ -199,45 +195,11 @@ class TumorHeatEqnFDM : public ScaFES::Problem<TumorHeatEqnFDM<CT,DIM>, CT, DIM>
                      std::vector<ScaFES::DataField<TT,DIM>> const& vOld,
                      ScaFES::Ntuple<int,DIM> const& idxNode,
                      int const& /*timestep*/) {
-        /* knownDf(0, idxNode). rho.     */
-        /* knownDf(1, idxNode). c.       */
-        /* knownDf(2, idxNode). lambda.  */
-        /* knownDf(3, idxNode). w.       */
-        /* knownDf(4, idxNode). q_dot_m. */
-        /* knownDf(5, idxNode). T.       */
-        double sphereCo =
-            (this->coordinates(idxNode).elem(0) - x0)
-            * (this->coordinates(idxNode).elem(0) - x0)
-            + (this->coordinates(idxNode).elem(1) - y0)
-            * (this->coordinates(idxNode).elem(1) - y0)
-            + (this->coordinates(idxNode).elem(2) - z0)
-            * (this->coordinates(idxNode).elem(2) - z0);
-        if (sphereCo <= (sphereRadius*sphereRadius)) {
-            /* Do nothing. */
-        } else {
-            vNew[0](idxNode) = vOld[0](idxNode);
-            for (std::size_t pp = 0; pp < DIM; ++pp) {
-                vNew[0](idxNode) += this->tau()
-                * (this->knownDf(2, idxNode)
-                    / (this->knownDf(0, idxNode) * this->knownDf(1, idxNode)))
-                * ((vOld[0](this->connect(idxNode, 2*pp))
-                   + vOld[0](this->connect(idxNode, 2*pp+1))
-                   - 2.0 * vOld[0](idxNode))
-                    / (this->gridsize(pp) * this->gridsize(pp)));
-            }
-            vNew[0](idxNode) += this->tau() * (
-                (RHO_BLOOD * C_BLOOD)
-                / (this->knownDf(0, idxNode) * this->knownDf(1, idxNode)))
-            * (this->knownDf(3, idxNode)
-            * (T_BLOOD - vOld[0](idxNode)));
-            vNew[0](idxNode) += this->tau() * (
-                (1.0/(this->knownDf(0, idxNode) * this->knownDf(1, idxNode))))
-            * this->knownDf(4, idxNode);
-        }
     }
 
     /** Updates all unknown fields at one given global border grid node.
      *  @param vNew Set of all unknown fields at new time step (return value).
+     *  @param vOld Set of all unknown fields at old time step.
      *  @param idxNode Index of given grid node.
      */
     template<typename TT>
@@ -245,41 +207,6 @@ class TumorHeatEqnFDM : public ScaFES::Problem<TumorHeatEqnFDM<CT,DIM>, CT, DIM>
                       std::vector<ScaFES::DataField<TT,DIM>>const& vOld,
                       ScaFES::Ntuple<int,DIM> const& idxNode,
                       int const& /*timestep*/) {
-        if (idxNode.elem(2) == (this->nNodes(2)-1)) {
-            /* knownDf(0, idxNode). rho.    */
-            /* knownDf(1, idxNode). c.      */
-            /* knownDf(2, idxNode). lambda. */
-            /* knownDf(3, idxNode). w.      */
-            /* knownDf(4, idxNode). q_dot_m.    */
-            /* knownDf(5, idxNode). T.      */
-            vNew[0](idxNode) = vOld[0](idxNode);
-            /* First and second dimension. */
-            for (std::size_t pp = 0; pp < 2; ++pp) {
-                vNew[0](idxNode) += this->tau()
-                * (this->knownDf(2, idxNode)
-                    / (this->knownDf(0, idxNode) * this->knownDf(1, idxNode)))
-                * ((vOld[0](this->connect(idxNode, 2*pp))
-                   + vOld[0](this->connect(idxNode, 2*pp+1))
-                   - 2.0 * vOld[0](idxNode))
-                    / (this->gridsize(pp) * this->gridsize(pp)));
-            }
-            /* Third dimension. */
-            vNew[0](idxNode) += this->tau()
-            * (this->knownDf(2, idxNode)
-                / (this->knownDf(0, idxNode) * this->knownDf(1, idxNode)))
-            * (((2.0 * this->gridsize(2) * (ALPHA_AMB/this->knownDf(2, idxNode)) * T_AMB)
-                + (2.0 * vOld[0](this->connect(idxNode, 2*2)))
-                - (2.0 * vOld[0](idxNode) * (this->gridsize(2) * (ALPHA_AMB/this->knownDf(2, idxNode)) + 1.0)))
-                / (this->gridsize(2) * this->gridsize(2)));
-            vNew[0](idxNode) += this->tau() * (
-                (RHO_BLOOD * C_BLOOD)
-                / (this->knownDf(0, idxNode) * this->knownDf(1, idxNode)))
-            * (this->knownDf(3, idxNode)
-            * (T_BLOOD - vOld[0](idxNode)));
-            vNew[0](idxNode) += this->tau() * (
-                (1.0/(this->knownDf(0, idxNode) * this->knownDf(1, idxNode))))
-            * this->knownDf(4, idxNode);
-        }
     }
 
     /** Updates (2nd cycle) all unknown fields at one given global inner grid node.
--- examples/TumorHeatEqnFDM/main.cpp
+++ examples/TumorHeatEqnFDM/main.cpp
@@ -1,5 +1,5 @@
 /* ScaFES
- * Copyright (c) 2011-2015, ZIH, TU Dresden, Federal Republic of Germany.
+ * Copyright (c) 2017, ZIH, TU Dresden, Federal Republic of Germany.
  * For details, see the files COPYING and LICENSE in the base directory
  * of the package.
  */
@@ -14,54 +14,33 @@ const int DIM = 3;
 int main(int argc, char *argv[]) {
     ScaFES::Parameters paramsCl(argc, argv);
     ScaFES::GridGlobal<DIM> gg(paramsCl);
-    std::vector<std::string> nameDatafield(6);
-    nameDatafield[0] = "rho";     /* rho (density) vector.                 */
-    nameDatafield[1] = "c";       /* c (specific heat capacity) vector.    */
-    nameDatafield[2] = "lambda";  /* lambda (thermal conductivity) vector. */
-    nameDatafield[3] = "w";       /* w (perfusion) vector.                 */
-    nameDatafield[4] = "q_dot_m"; /* q_dot_m (metabolism) vector.          */
-    nameDatafield[5] = "T";       /* Temperature vector.                   */
-    std::vector<int> stencilWidth(6);
+    std::vector<std::string> nameDatafield(2);
+    nameDatafield[0] = "w";       /* blood perfusion rate. */
+    nameDatafield[1] = "T";       /* Temperature.          */
+    std::vector<int> stencilWidth(2);
     stencilWidth[0] = 0;
-    stencilWidth[1] = 0;
-    stencilWidth[2] = 0;
-    stencilWidth[3] = 0;
-    stencilWidth[4] = 0;
-    stencilWidth[5] = 1;
-    std::vector<bool> isKnownDf(6);
+    stencilWidth[1] = 1;
+    std::vector<bool> isKnownDf(2);
     isKnownDf[0] = true;
-    isKnownDf[1] = true;
-    isKnownDf[2] = true;
-    isKnownDf[3] = true;
-    isKnownDf[4] = true;
-    isKnownDf[5] = false;
-    std::vector<int> nLayers(6);
+    isKnownDf[1] = false;
+    std::vector<int> nLayers(2);
     nLayers[0] = 0;
     nLayers[1] = 0;
-    nLayers[2] = 0;
-    nLayers[3] = 0;
-    nLayers[4] = 0;
-    nLayers[5] = 0;
-    std::vector<double> defaultValue(6);
+    std::vector<double> defaultValue(2);
     defaultValue[0] = 0.0;
     defaultValue[1] = 0.0;
-    defaultValue[2] = 0.0;
-    defaultValue[3] = 0.0;
-    defaultValue[4] = 0.0;
-    defaultValue[5] = 0.0;
-    std::vector<ScaFES::WriteHowOften> writeToFile(6, ScaFES::WriteHowOften::LIKE_GIVEN_AT_CL);
-    std::vector<bool> computeError(6);
+    std::vector<ScaFES::WriteHowOften> writeToFile(2);
+    writeToFile[0] = ScaFES::WriteHowOften::NEVER;
+    writeToFile[1] = ScaFES::WriteHowOften::LIKE_GIVEN_AT_CL;
+    std::vector<bool> computeError(2);
     computeError[0] = false;
     computeError[1] = false;
-    computeError[2] = false;
-    computeError[3] = false;
-    computeError[4] = false;
-    computeError[5] = false;
     std::vector<double> geomparamsInit;
 
-    TumorHeatEqnFDM<double, DIM> ppp(paramsCl, gg, false, nameDatafield, stencilWidth,
-                                isKnownDf, nLayers, defaultValue, writeToFile,
-                                computeError, geomparamsInit);
+    TumorHeatEqnFDM<double, DIM> ppp(paramsCl, gg, false, nameDatafield,
+                                     stencilWidth, isKnownDf, nLayers,
+                                     defaultValue, writeToFile, computeError,
+                                     geomparamsInit);
     ppp.iterateOverTime();
 
     return 0;
