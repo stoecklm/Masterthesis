270f51d584704401a396e70351dfca1d5a3169a2
Martin Stoeckl <martin.stoeckl@mailbox.tu-dresden.de>
2017-12-16 15:05:26 +0100

    Updated python script to run simulation
    Moved most code from main function to new functions which are called in main function.
    Variables from ini files are now stored in a dict.

--- examples/TumorHeatEqnFDM/startSimulation.py
+++ examples/TumorHeatEqnFDM/startSimulation.py
@@ -1,13 +1,103 @@
 import configparser
 import math
+import netCDF4 as nc
+import numpy as np
 import os
 import subprocess
 import sys
 
+params = {
+    'NAME_CONFIGFILE' : '',
+    'SPACE_DIM' : 0,
+    'GRIDSIZE_GLOBAL' : 0.0,
+    'COORD_NODE_FIRST_DIM1' : 0.0,
+    'COORD_NODE_FIRST_DIM2' : 0.0,
+    'COORD_NODE_FIRST_DIM3' : 0.0,
+    'COORD_NODE_LAST_DIM1' : 0.0,
+    'COORD_NODE_LAST_DIM2' : 0.0,
+    'COORD_NODE_LAST_DIM3' : 0.0,
+    'START_TIME' : 0,
+    'END_TIME' : 0,
+    'DELTA_TIME' : 0.0,
+    'N_SNAPSHOTS': 0,
+    'N_TIMESTEPS' : 0,
+    'N_NODES_DIM1' : 0,
+    'N_NODES_DIM2' : 0,
+    'N_NODES_DIM3' : 0,
+}
+
+def parseConfigFile():
+    global params
+
+    config = configparser.ConfigParser()
+    config.read(params['NAME_CONFIGFILE'])
+
+    params['SPACE_DIM'] = config['Dimension'].getint('SPACE_DIM')
+
+    params['GRIDSIZE_GLOBAL'] = config['Geometry'].getfloat('GRIDSIZE_GLOBAL')
+    params['COORD_NODE_FIRST_DIM1'] = config['Geometry'].getfloat('COORD_NODE_FIRST_DIM1')
+    params['COORD_NODE_FIRST_DIM2'] = config['Geometry'].getfloat('COORD_NODE_FIRST_DIM2')
+    params['COORD_NODE_FIRST_DIM3'] = config['Geometry'].getfloat('COORD_NODE_FIRST_DIM3')
+    params['COORD_NODE_LAST_DIM1'] = config['Geometry'].getfloat('COORD_NODE_LAST_DIM1')
+    params['COORD_NODE_LAST_DIM2'] = config['Geometry'].getfloat('COORD_NODE_LAST_DIM2')
+    params['COORD_NODE_LAST_DIM3'] = config['Geometry'].getfloat('COORD_NODE_LAST_DIM3')
+
+    params['START_TIME'] = config['Time'].getint('START_TIME')
+    params['END_TIME']= config['Time'].getint('END_TIME')
+    params['DELTA_TIME'] = config['Time'].getfloat('DELTA_TIME')
+
+    params['N_SNAPSHOTS'] = config['Output'].getint('N_SNAPSHOTS')
+
+def calcVariables():
+    global params
+
+    N_NODES_DIM1 = (params['COORD_NODE_LAST_DIM1'] - params['COORD_NODE_FIRST_DIM1'])/params['GRIDSIZE_GLOBAL']
+    params['N_NODES_DIM1'] = int(math.ceil(N_NODES_DIM1))
+    N_NODES_DIM2 = (params['COORD_NODE_LAST_DIM2'] - params['COORD_NODE_FIRST_DIM2'])/params['GRIDSIZE_GLOBAL']
+    params['N_NODES_DIM2'] = int(math.ceil(N_NODES_DIM2))
+    N_NODES_DIM3 = (params['COORD_NODE_LAST_DIM3'] - params['COORD_NODE_FIRST_DIM3'])/params['GRIDSIZE_GLOBAL']
+    params['N_NODES_DIM3'] = int(math.ceil(N_NODES_DIM3))
+
+    N_TIMESTEPS = (params['END_TIME'] - params['START_TIME'])/params['DELTA_TIME']
+    params['N_TIMESTEPS'] = int(math.ceil(N_TIMESTEPS))
+
+    if params['N_SNAPSHOTS'] > params['N_TIMESTEPS']:
+        params['N_SNAPSHOTS'] = params['N_TIMESTEPS']
+
+def setEnvironmentVariables():
+    global params
+
+    os.putenv("SCAFESRUN_N_TIMESTEPS", str(params['N_TIMESTEPS']))
+    os.putenv("SCAFESRUN_N_SNAPSHOTS", str(params['N_SNAPSHOTS']))
+    os.putenv("SCAFESRUN_START_TIME", str(params['START_TIME']))
+    os.putenv("SCAFESRUN_END_TIME", str(params['END_TIME']))
+
+    os.putenv("SCAFESRUN_NAME_CONFIGFILE", params['NAME_CONFIGFILE'])
+    os.putenv("SCAFESRUN_SPACE_DIM", str(params['SPACE_DIM']))
+
+    COORD_NODE_FIRST = str(params['COORD_NODE_FIRST_DIM1'])
+    COORD_NODE_LAST = str(params['COORD_NODE_LAST_DIM1'])
+    N_NODES = str(params['N_NODES_DIM1'])
+
+    if params['SPACE_DIM'] == 2:
+        COORD_NODE_FIRST += "x" + str(params['COORD_NODE_FIRST_DIM2'])
+        COORD_NODE_LAST += "x" + str(params['COORD_NODE_LAST_DIM2'])
+        N_NODES += "x" + str(params['N_NODES_DIM2'])
+    if params['SPACE_DIM'] == 3:
+        COORD_NODE_FIRST += "x" + str(params['COORD_NODE_FIRST_DIM3'])
+        COORD_NODE_LAST += "x" + str(params['COORD_NODE_LAST_DIM3'])
+        N_NODES += "x" + str(params['N_NODES_DIM3'])
+
+    os.putenv("SCAFESRUN_COORD_NODE_FIRST", COORD_NODE_FIRST)
+    os.putenv("SCAFESRUN_COORD_NODE_LAST", COORD_NODE_LAST)
+    os.putenv("SCAFESRUN_N_NODES", N_NODES)
+
 def main():
+    global params
+    # Check if path to configfile is provided.
     if len(sys.argv) > 1:
         if os.path.isfile(sys.argv[1]) == True:
-            NAME_CONFIGFILE = sys.argv[1]
+            params['NAME_CONFIGFILE'] = sys.argv[1]
         else:
             print(sys.argv[1], "does not exist.")
             print("Usage: python3", sys.argv[0], "<PATH/TO/CONFIGFILE>")
@@ -19,59 +109,9 @@ def main():
         print("Aborting.")
         exit()
 
-    config = configparser.ConfigParser()
-    config.read(NAME_CONFIGFILE)
-
-    SPACE_DIM = config['Dimension'].getint('SPACE_DIM')
-
-    GRIDSIZE_GLOBAL = config['Geometry'].getfloat('GRIDSIZE_GLOBAL')
-    COORD_NODE_FIRST_DIM1 = config['Geometry'].getfloat('COORD_NODE_FIRST_DIM1')
-    COORD_NODE_FIRST_DIM2 = config['Geometry'].getfloat('COORD_NODE_FIRST_DIM2')
-    COORD_NODE_FIRST_DIM3 = config['Geometry'].getfloat('COORD_NODE_FIRST_DIM3')
-    COORD_NODE_LAST_DIM1 = config['Geometry'].getfloat('COORD_NODE_LAST_DIM1')
-    COORD_NODE_LAST_DIM2 = config['Geometry'].getfloat('COORD_NODE_LAST_DIM2')
-    COORD_NODE_LAST_DIM3 = config['Geometry'].getfloat('COORD_NODE_LAST_DIM3')
-
-    N_NODES_DIM1 = (COORD_NODE_LAST_DIM1 - COORD_NODE_FIRST_DIM1)/GRIDSIZE_GLOBAL
-    N_NODES_DIM1 = int(math.ceil(N_NODES_DIM1))
-    N_NODES_DIM2 = (COORD_NODE_LAST_DIM2 - COORD_NODE_FIRST_DIM2)/GRIDSIZE_GLOBAL
-    N_NODES_DIM2 = int(math.ceil(N_NODES_DIM2))
-    N_NODES_DIM3 = (COORD_NODE_LAST_DIM3 - COORD_NODE_FIRST_DIM3)/GRIDSIZE_GLOBAL
-    N_NODES_DIM3 = int(math.ceil(N_NODES_DIM3))
-
-    START_TIME = config['Time'].getint('START_TIME')
-    END_TIME = config['Time'].getint('END_TIME')
-    DELTA_TIME = config['Time'].getfloat('DELTA_TIME')
-
-    N_TIMESTEPS = (END_TIME - START_TIME)/DELTA_TIME
-    N_TIMESTEPS = int(math.ceil(N_TIMESTEPS))
-
-    N_SNAPSHOTS = config['Output'].getint('N_SNAPSHOTS')
-    if N_SNAPSHOTS > N_TIMESTEPS:
-        N_SNAPSHOTS = N_TIMESTEPS
-
-    os.putenv("SCAFESRUN_N_TIMESTEPS", str(N_TIMESTEPS))
-    os.putenv("SCAFESRUN_N_SNAPSHOTS", str(N_SNAPSHOTS))
-    os.putenv("SCAFESRUN_START_TIME", str(START_TIME))
-    os.putenv("SCAFESRUN_END_TIME", str(END_TIME))
-
-    if SPACE_DIM == 2:
-        tmp = str(COORD_NODE_FIRST_DIM1) + "x" + str(COORD_NODE_FIRST_DIM2)
-        os.putenv("SCAFESRUN_COORD_NODE_FIRST", tmp)
-        tmp = str(COORD_NODE_LAST_DIM1) + "x" + str(COORD_NODE_LAST_DIM2)
-        os.putenv("SCAFESRUN_COORD_NODE_LAST", tmp)
-        tmp = str(N_NODES_DIM1) + "x" + str(N_NODES_DIM2)
-        os.putenv("SCAFESRUN_N_NODES", tmp)
-    if SPACE_DIM == 3:
-        tmp = str(COORD_NODE_FIRST_DIM1) + "x" + str(COORD_NODE_FIRST_DIM2) + "x" + str(COORD_NODE_FIRST_DIM3)
-        os.putenv("SCAFESRUN_COORD_NODE_FIRST", tmp)
-        tmp = str(COORD_NODE_LAST_DIM1) + "x" + str(COORD_NODE_LAST_DIM2) + "x" + str(COORD_NODE_LAST_DIM3)
-        os.putenv("SCAFESRUN_COORD_NODE_LAST", tmp)
-        tmp = str(N_NODES_DIM1) + "x" + str(N_NODES_DIM2) + "x" + str(N_NODES_DIM3)
-        os.putenv("SCAFESRUN_N_NODES", tmp)
-    os.putenv("SCAFESRUN_NAME_CONFIGFILE", NAME_CONFIGFILE)
-    os.putenv("SCAFESRUN_SPACE_DIM", str(SPACE_DIM))
-
+    parseConfigFile()
+    calcVariables()
+    setEnvironmentVariables()
     subprocess.call("./RUN_HELPER.sh")
 
 if __name__ == '__main__':
