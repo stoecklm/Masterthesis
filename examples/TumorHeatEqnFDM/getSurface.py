67e25fb8527e8cf10ef451cdc7ec90f04415371d
Martin Stoeckl <martin.stoeckl@mailbox.tu-dresden.de>
2018-01-21 14:03:39 +0100

    Updated post processing script for extracting surface data
    Script will test if file has .nc extension to somehow make sure it is a netCDF file.
    Output of script (i.e. surface data) will be named after input file.

--- examples/TumorHeatEqnFDM/getSurface.py
+++ examples/TumorHeatEqnFDM/getSurface.py
@@ -5,12 +5,17 @@ import subprocess
 import sys
 
 def main():
-    global params
-    # Check if path to configfile is provided and
-    # if file exists.
+    # Check if path to netCDF file (i.e. results) is provided,
+    # if file exists and if file has .nc extension.
     if len(sys.argv) > 1:
         if os.path.isfile(sys.argv[1]) == True:
-            file_name = sys.argv[1]
+            if os.path.splitext(sys.argv[1])[1] == '.nc':
+                filepath = sys.argv[1]
+            else:
+                print(sys.argv[1], 'does not have .nc extension.')
+                print('Usage: python3', sys.argv[0], '<PATH/TO/FILE>')
+                print('Aborting.')
+                exit()
         else:
             print(sys.argv[1], 'does not exist.')
             print('Usage: python3', sys.argv[0], '<PATH/TO/FILE>')
@@ -22,7 +27,9 @@ def main():
         print('Aborting.')
         exit()
 
-    nc_file = nc.Dataset(file_name)
+    print('Read data from {0}.'.format(filepath))
+
+    nc_file = nc.Dataset(filepath)
     dim0 = nc_file.dimensions['nNodes_0'].size
     dim1 = nc_file.dimensions['nNodes_1'].size
     dim2 = nc_file.dimensions['nNodes_2'].size
@@ -34,12 +41,19 @@ def main():
 
     nc_file.close()
 
-    text_file = open('surface.dat', 'w')
+    filepath = os.path.splitext(filepath)[0]
+    filepath += '_surface.dat'
+
+    text_file = open(filepath, 'w')
+
+    print('Write surface data to {0}.'.format(filepath))
 
     for elem_y in range(0, a.shape[0]):
         for elem_x in range(0, a.shape[1]):
             text_file.write('{0} {1} {2}\n'.format(str(elem_x), str(elem_y), str(a[elem_y, elem_x])))
         text_file.write('\n')
+
+    print('Done.')
 
 if __name__ == '__main__':
     main()
