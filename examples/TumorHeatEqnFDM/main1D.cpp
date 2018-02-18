5fded2d162e49eaf1925dfb4ce61d354c7c4e04e
Martin Stoeckl <martin.stoeckl@mailbox.tu-dresden.de>
2018-02-18 15:43:00 +0100

    Removed dummy datafield from TumorHeatEqn example
    At least one known datafield was necessary before the fix.
    This was fixed in a previous commit (9a4090b).
    Removed the dummy datafield from main files in example.

--- examples/TumorHeatEqnFDM/main1D.cpp
+++ examples/TumorHeatEqnFDM/main1D.cpp
@@ -14,31 +14,23 @@ const int DIM = 1;
 int main(int argc, char *argv[]) {
     ScaFES::Parameters paramsCl(argc, argv);
     ScaFES::GridGlobal<DIM> gg(paramsCl);
-    std::vector<std::string> nameDatafield(2);
-    nameDatafield[0] = "dummy";  /* Will not be used.                          */
-    nameDatafield[1] = "T";      /* Temperature.                               */
-    std::vector<int> stencilWidth(2);
-    stencilWidth[0] = 0;
-    stencilWidth[1] = 1;
-    std::vector<bool> isKnownDf(2);
-    isKnownDf[0] = true;
-    isKnownDf[1] = false;
-    std::vector<int> nLayers(2);
+    std::vector<std::string> nameDatafield(1);
+    nameDatafield[0] = "T";      /* Temperature. */
+    std::vector<int> stencilWidth(1);
+    stencilWidth[0] = 1;
+    std::vector<bool> isKnownDf(1);
+    isKnownDf[0] = false;
+    std::vector<int> nLayers(1);
     nLayers[0] = 0;
-    nLayers[1] = 0;
-    std::vector<double> defaultValue(2);
+    std::vector<double> defaultValue(1);
     defaultValue[0] = 0.0;
-    defaultValue[1] = 0.0;
-    std::vector<ScaFES::WriteHowOften> writeToFile(2);
-    writeToFile[0] = ScaFES::WriteHowOften::NEVER;
-    writeToFile[1] = ScaFES::WriteHowOften::AT_END;
-    std::vector<bool> computeError(2);
+    std::vector<ScaFES::WriteHowOften> writeToFile(1);
+    writeToFile[0] = ScaFES::WriteHowOften::AT_END;
+    std::vector<bool> computeError(1);
     computeError[0] = false;
-    computeError[1] = false;
+    std::vector<bool> checkConvergence(1);
+    checkConvergence[0] = true;
     std::vector<double> geomparamsInit;
-    std::vector<bool> checkConvergence(2);
-    checkConvergence[0] = false;
-    checkConvergence[1] = true;
 
     boost::property_tree::ptree pt;
     boost::property_tree::ini_parser::read_ini(paramsCl.nameConfigFile(), pt);
