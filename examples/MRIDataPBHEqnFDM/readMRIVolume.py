383e2f8196214d8abddbad1610b8ec66825ea125
Martin Stoeckl <martin.stoeckl@mailbox.tu-dresden.de>
2018-04-03 16:38:24 +0200

    Updated pre-processing script
    Added function to extract smaller array out of big array.

--- examples/MRIDataPBHEqnFDM/readMRIVolume.py
+++ examples/MRIDataPBHEqnFDM/readMRIVolume.py
@@ -5,16 +5,27 @@ import netCDF4 as nc
 import numpy as np
 import nrrd
 
-def save_volume_as_netcdf(data):
-    nc_file = nc.Dataset('volume.nc', 'w', format='NETCDF3_CLASSIC')
+def extract_tumor_from_volume(data, start, end):
+    length = (end[0]-start[0], end[1]-start[1], end[2]-start[2])
+    num_elem = length[0]*length[1]*length[2]
+    print(length)
+    new_data = np.arange(num_elem).reshape(length)
+    new_data = data[start[0]:end[0]+1,
+                    start[1]:end[1]+1,
+                    start[2]:end[2]+1]
+
+    return new_data
+
+
+def save_volume_as_netcdf(data, filename):
+    nc_file = nc.Dataset(filename, 'w', format='NETCDF3_CLASSIC')
     nNodes = nc_file.createDimension('nNodes_0', data.shape[0])
     nNodes = nc_file.createDimension('nNodes_1', data.shape[1])
     nNodes = nc_file.createDimension('nNodes_2', data.shape[2])
     time = nc_file.createDimension('time')
     brain = nc_file.createVariable('brain', 'f8', ('time', 'nNodes_2',
                                                   'nNodes_1', 'nNodes_0'))
-
-    brain[0] = data.T
+    brain[0,:,:,:] = data.T
 
     nc_file.close()
 
@@ -48,7 +59,15 @@ def main():
 
     data = read_volume(filepath)
 
-    save_volume_as_netcdf(data)
+    save_volume_as_netcdf(data, 'whole_mri_volume.nc')
+
+    #start = (nNodes_0, nNodes_1, nNodes_2)
+    start = (90,45,20)
+    end = (150,100,80)
+    # end = (224, 256, 176)
+    tumor = extract_tumor_from_volume(data, start, end)
+
+    save_volume_as_netcdf(tumor, 'tumor_mri.nc')
 
     print('Done.')
 
