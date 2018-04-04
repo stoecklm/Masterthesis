b7595cf320944a4735c6d479dbb6e007e216150d
Martin Stoeckl <martin.stoeckl@mailbox.tu-dresden.de>
2018-04-04 09:36:47 +0200

    Updated pre-processing script for mri volume
    Added some sort of tumor segmentation.
    Saves tumor to netCDF with 0/1 values for brain/tumor.

--- examples/MRIDataPBHEqnFDM/readMRIVolume.py
+++ examples/MRIDataPBHEqnFDM/readMRIVolume.py
@@ -5,14 +5,33 @@ import netCDF4 as nc
 import numpy as np
 import nrrd
 
+def stats_from_tumor(data):
+    print('Mean: {}.'.format(np.mean(data)))
+    print('Min: {}.'.format(np.min(data)))
+    print('Max: {}.'.format(np.max(data)))
+    print('StdDev: {}.'.format(np.std(data)))
+
+def binary_data(data):
+    bin_data = np.zeros(data.shape)
+    mean = np.mean(data)
+    std_dev = np.std(data)
+
+    bin_data = np.where(data > mean + std_dev, 1, 0)
+
+    save_volume_as_netcdf(bin_data, 'bin_data.nc')
+
+
 def extract_tumor_from_volume(data, start, end):
-    length = (end[0]-start[0], end[1]-start[1], end[2]-start[2])
+    length = (end[0]-start[0]+1, end[1]-start[1]+1, end[2]-start[2]+1)
     num_elem = length[0]*length[1]*length[2]
     print(length)
     new_data = np.arange(num_elem).reshape(length)
+    print(new_data.shape)
     new_data = data[start[0]:end[0]+1,
                     start[1]:end[1]+1,
                     start[2]:end[2]+1]
+    print(new_data.shape)
+    print('....')
 
     return new_data
 
@@ -23,7 +42,7 @@ def save_volume_as_netcdf(data, filename):
     nNodes = nc_file.createDimension('nNodes_1', data.shape[1])
     nNodes = nc_file.createDimension('nNodes_2', data.shape[2])
     time = nc_file.createDimension('time')
-    brain = nc_file.createVariable('brain', 'f8', ('time', 'nNodes_2',
+    brain = nc_file.createVariable('mri', 'i2', ('time', 'nNodes_2',
                                                   'nNodes_1', 'nNodes_0'))
     brain[0,:,:,:] = data.T
 
@@ -68,6 +87,10 @@ def main():
     tumor = extract_tumor_from_volume(data, start, end)
 
     save_volume_as_netcdf(tumor, 'tumor_mri.nc')
+
+    stats_from_tumor(tumor)
+
+    binary_data(tumor)
 
     print('Done.')
 
